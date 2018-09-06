8dd01f3109aa235547a17a10518afb24b0a2155a
gselvamanimech <42578405+gselvamanimech@users.noreply.github.com>
2018-09-06 10:11:45 +0530

    Update MailSending.java

--- BankApp/src/admin/MailSending.java
+++ BankApp/src/admin/MailSending.java
@@ -3,6 +3,6 @@ package admin;
 
 public class MailSending {
 	public static void main(String[] args) {
-		 MailServices.send("selvamanistar@gmail.com","maniselva","selvamanistar@gmail.com","Your Request Has Been Submitted","Your Card will be Sent to you within 2 days");  
+		 MailServices.send("Sender Mail","","Receiver Mail","Subject","Message");  
 	}
 }
